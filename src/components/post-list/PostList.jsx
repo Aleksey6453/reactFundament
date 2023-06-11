05aaa86d06b1cbfa08b60fdf7b3bb303fd393961
Aleksey6453 <login6453@gmail.com>
2023-06-11 15:53:32 +0300

    add animation from transition group

--- src/components/post-list/PostList.jsx
+++ src/components/post-list/PostList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PostItem from '../postItem/PostItem'
 import styles from './PostList.module.css'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 
 const PostList = ({posts, title, remove}) => {
@@ -8,7 +9,7 @@ const PostList = ({posts, title, remove}) => {
       return(
         <div><h2> Were is empty now :( </h2>
           <br />
-          <img src="/1.jpg" alt="photo" style={{width: '100%'}}/>
+             <img src="/1.jpg" alt="photo" style={{width: '100%'}} key={22} classNames="item"/> 
           </div>
       )
   }
@@ -16,9 +17,18 @@ const PostList = ({posts, title, remove}) => {
   return (
     <div className={styles.post_list}>
       <h1>{title}</h1>
-      {posts.map((post, index) => 
-        <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-      )}
+      <TransitionGroup>
+        {posts.map((post, index) => 
+            <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+            >
+                <PostItem remove={remove} number={index + 1} post={post}  />
+            </CSSTransition>
+        )}
+      </TransitionGroup>
+     
     </div>
   )
 }
